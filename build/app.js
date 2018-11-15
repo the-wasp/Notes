36fdbaa67a4d34899068c6987c925ad18650142b
Omoboye Adedamola <dayemmydecibel@gmail.com>
2018-11-15 21:24:28 +0100

    worked on the bootstrap

---
+++ build/app.js
@@ -0,0 +1,23 @@
+
+const addButton = document.querySelector('#addNote');
+const txtInput = document.querySelector('#txtInput');
+
+
+addButton.addEventListener('click', function(){
+    
+});
+
+txtInput.addEventListener('focus', function(){
+
+});
+
+
+
+
+
+
+
+
+
+
+
